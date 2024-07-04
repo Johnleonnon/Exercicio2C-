8dbdf1b435c46d8b71779cb334cd2ee0dc4c2c77
John Leonnon <jowjohnleonnon@gmail.com>
2024-07-04 11:12:51 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio, pi = 3.14159;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = pi * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4"), CultureInfo.InvariantCulture);
+        }
+    }
+}
